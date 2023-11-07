958e464fc066bcdf5892bc277a3a68a161ab66c6
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-07 20:40:26 +0400

    Added changes files in the folder 'HW3'

--- src/HW3/ControlData.java
+++ src/HW3/ControlData.java
@@ -1,6 +1,41 @@
 package HW3;
 
 public class ControlData {
-    private static void ControlException ()
+    private static void ControlException (String lastName, String firstName, String patronymic, String birthDate, long phoneNumber, char gender){
+
+        if(!lastName.matches("[a-zA-Z]+") || !firstName.matches("[a-zA-Z]+") || !patronymic.matches("[a-zA-Z]+"))
+        {
+            throw new IllegalArgumentException("Данные Ф.И.О. введены некорректно");
+        }
+        if(!birthDate.matches("\\d{2}.\\d{2}.\\d{4}"))
+        {
+            throw new IllegalArgumentException("Информация неправильного формата");
+        }
+
+        String[] dataParts = birthDate.split("\\.");
+        int day = Integer.parseInt(dataParts[0]);
+        int month = Integer.parseInt(dataParts[1]);
+
+        if (day < 1 || day > 31 || month < 1 || month > 12)
+        {
+            throw new IllegalArgumentException("Введены неверные числа даты рождения");
+        }
+        // Validate phoneNumber (String)
+        // if(!phoneNumber.matches("^\\+(\\d{1,3}(\\(\\d{1,3}\\))?)(?:[0-9] ?){6,14}[0-9]$"))
+        // {
+        //     throw new IllegalArgumentException("Incorrect Phone Number");
+        // }
+        // Validate phoneNumber (long)
+        if (phoneNumber < 70000000000L || phoneNumber > 79999999999L)
+        {
+            throw new IllegalArgumentException("Incorrect Phone Number. The phone number should be within the range of +70000000000 to +79999999999.");
+        }
+
+        // Validate gender (Char)
+        if(!String.valueOf(gender).equalsIgnoreCase("f") && !String.valueOf(gender).equalsIgnoreCase("m"))
+        {
+            throw new IllegalArgumentException("Incorrect gender");
+        }
+    }
 
 }
--- src/HW3/ParserData.java
+++ src/HW3/ParserData.java
@@ -13,52 +13,18 @@ public class ParserData {
         String birthDate = data[3];
         long phoneNumber = Long.parseLong(data[4]);
         char gender = data[5].charAt(0);
-//        //Check validate data
-//        validateUserData(SurName, FirstName, Patronymic, birthDate, phoneNumber, gender);
+
+        validateUserData(lastName, firstName, patronymic, birthDate, phoneNumber, gender);
     }
 
 
-    //    // Create scanner
-//    Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter the data in format: SurName, FirstName, Patronymic, Date of birth, Phone Number, You gender: M or F");
-//    String inputScanner = scanner.nextLine();
-//    //Check userData to 6
-//        try {
-//        // Split string
-//        String[] userData = inputScanner.trim().split(" ");
-//        // Cheak to correct number of fields
-//        if (userData.length != 6)
-//        {
-//            throw new IllegalArgumentException
-//                    ("The incorrect number of fields is filled in, please fill in the correct number of fields, which should not exceed six or be less than six.");
-//        }
-//        // Add userData
-//        String SurName = userData[0];
-//        String FirstName = userData[1];
-//        String Patronymic = userData[2];
-//        String birthDate = userData[3];
-//        // String phoneNumber = userData[4];
-//        long phoneNumber = Long.parseLong(userData[4]);
-//        // String gender = userData[5];
-//        char gender = userData[5].charAt(0);
+
+
 //        //Check validate userData
 //        validateUserData(SurName, FirstName, Patronymic, birthDate, phoneNumber, gender);
 //        //Record userData
 //        recUserData(SurName, FirstName, Patronymic, birthDate, phoneNumber, gender);
 //
 //        System.out.println("User Data Successfully Saved");
-//    }
-//        catch (IllegalArgumentException e)
-//    {
-//        System.out.println("Error: " + e.getMessage());
-//    }
-//        catch (IOException e)
-//    {
-//        System.out.println("Error: Record or Read file");
-//        e.printStackTrace();
-//    }
-//        finally
-//    {
-//        scanner.close();
-//    }
+
 }
