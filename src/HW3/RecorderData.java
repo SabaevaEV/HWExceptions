ac60625e0dba5922d785bf260ab626a6bd1ac5a6
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-07 20:02:38 +0400

    Added changes 'RecorderData' files in the folder 'HW3'

--- src/HW3/RecorderData.java
+++ src/HW3/RecorderData.java
@@ -1,5 +1,18 @@
 package HW3;
 
-public class RecorderData {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class RecorderData {
+    private static void Recorder(String lastName, String firstName, String patronymic, String birthDate, long phoneNumber, char gender) throws IOException
+    {
+        // Create fille
+        String newFile = lastName + ".txt";
+//        BufferedWriter writer = new BufferedWriter(new FileWriter(newFile, true)
+        try (FileWriter writer = new FileWriter (newFile, true))
+        {
+            writer.write("Фамилия: " + lastName + ", Имя: " + firstName + ", Отчество: " + patronymic +
+                    ",  Дата рождения: " + birthDate + ", Номер телефона: " + phoneNumber + ", Под: " + gender + ";" + "\n");
+        }
+    }
 }
