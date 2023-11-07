c8d5572ce418cb0317f628a80f887f9fe5f4f31c
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-07 22:21:27 +0400

    Added final changes files in the folder 'HW3'

--- src/HW3/InputData.java
+++ src/HW3/InputData.java
@@ -4,24 +4,19 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class InputData {
-    public String[] inputData() throws IOException {
-        Scanner scanner = new Scanner(System.in);
+    public String[] inputData() {
 
-        try {
-            System.out.println("Введите следующие данные через пробел: \n "
-                    + "Фамилия Имя Отчество, дату рождения в формате - dd.mm.yyyy, номер телефона - цифры, пол - f/m ): \n");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите следующие данные через пробел: \n"
+                    + "Фамилия Имя Отчество, дату рождения в формате - dd.mm.yyyy, номер телефона - цифры, пол - f/m :");
             String inputData = scanner.nextLine();
             String[] arrayData = inputData.split(" ");
-            if (arrayData.length != 6){
+            if (arrayData.length != 6) {
                 throw new IllegalArgumentException("Введено неверное количество данных, повторите ввод");
             }
             return arrayData;
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("Ошибка " + e.getMessage());
-        }
-        finally
-        {
-            scanner.close();
         }
         return null;
     }
